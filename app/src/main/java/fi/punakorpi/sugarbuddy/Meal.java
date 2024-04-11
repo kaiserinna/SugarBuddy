59e5b099eaafc1453a6bd15c4c937f68604e7a7c
Kaisa Punakorpi <kaisa.punakorpi@gmail.com>
2024-04-11 15:22:47 +0300

    Meal dataclass started

--- app/src/main/java/fi/punakorpi/sugarbuddy/Meal.java
+++ app/src/main/java/fi/punakorpi/sugarbuddy/Meal.java
@@ -31,4 +31,59 @@ public class Meal {
         float correctiveBloodSugar = bloodSugar - 6; //voi palauttaa - merkkisen arvon, silloin vs on pienempi kuin 6 ja insulinTotalista pitää vähentää oikea määrä insuliinia
         profile.getCarbFactor(mealTypeIndex)*/
 
+    public ArrayList<Food> getFoods() {
+        return foods;
     }
+
+    public void setFoods(ArrayList<Food> foods) {
+        this.foods = foods;
+    }
+
+    public String getMealType() {
+        return mealType;
+    }
+
+    public void setMealType(String mealType) {
+        this.mealType = mealType;
+    }
+
+    public int getMealIndexFromProfile() {
+        return mealIndexFromProfile;
+    }
+
+    public void setMealIndexFromProfile(int mealIndexFromProfile) {
+        this.mealIndexFromProfile = mealIndexFromProfile;
+    }
+
+    public float getBloodSugar() {
+        return bloodSugar;
+    }
+
+    public void setBloodSugar(float bloodSugar) {
+        this.bloodSugar = bloodSugar;
+    }
+
+    public float getCarbsTotal() {
+        return carbsTotal;
+    }
+
+    public void setCarbsTotal(float carbsTotal) {
+        this.carbsTotal = carbsTotal;
+    }
+
+    public String getInsulinDoseText() {
+        return insulinDoseText;
+    }
+
+    public void setInsulinDoseText(String insulinDoseText) {
+        this.insulinDoseText = insulinDoseText;
+    }
+
+    public float getInsulinDoseTotal() {
+        return insulinDoseTotal;
+    }
+
+    public void setInsulinDoseTotal(float insulinDoseTotal) {
+        this.insulinDoseTotal = insulinDoseTotal;
+    }
+}
--- app/src/main/java/fi/punakorpi/sugarbuddy/MealListAdapter.java
+++ app/src/main/java/fi/punakorpi/sugarbuddy/MealListAdapter.java
@@ -1,34 +1,39 @@
 package fi.punakorpi.sugarbuddy;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class MealListAdapter extends RecyclerView.Adapter<MealItemViewHolder> {
     private Context context;
-    private Meal mealList;
+    private ArrayList<Meal> mealList = new ArrayList<>();
 
-    public MealListAdapter(Context context) {
+    public MealListAdapter(Context context, ArrayList<Meal> mealList) {
         this.context = context;
-    //    this.mealList = Meal.
+        this.mealList = mealList
     }
 
     @NonNull
     @Override
     public MealItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new MealItemViewHolder(LayoutInflater.from(context).inflate(R.layout.meal_item_view, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull MealItemViewHolder holder, int position) {
+        //TODO: holder.meal:::::setText(mealList.get(position).getMealType());
+        holder.setText
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mealList.size();
     }
 
 
